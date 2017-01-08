8e3edf89d44dc5933cb6c1d3c925d9904972c6dc
Sebastian Majewski <sebastian.majewski.it@gmail.com>
2017-01-08 03:02:07 +0100

    Generated code

---
+++ ASPProjekt/ASPProjekt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPProjekt.Startup))]
+namespace ASPProjekt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
